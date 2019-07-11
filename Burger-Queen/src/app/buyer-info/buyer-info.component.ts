9e9f9426767dda55456ba6071e1817be7d4a45c3
frankynztein <arellanfranciree@gmail.com>
2019-07-10 22:49:16 -0500

    Se oculta componente buyer-info con el event binding

--- Burger-Queen/src/app/buyer-info/buyer-info.component.ts
+++ Burger-Queen/src/app/buyer-info/buyer-info.component.ts
@@ -9,12 +9,14 @@ import { DataService } from '../data.service';
 export class BuyerInfoComponent implements OnInit {
 
   buyerName:string = '';
+  hide:boolean = true;
 
   constructor(private data: DataService) { }
   
   takeOrder() {
     console.log(this.buyerName);
     this.data.changeBuyerName(this.buyerName)
+    this.hide = !this.hide;
   }
   
   ngOnInit() {
