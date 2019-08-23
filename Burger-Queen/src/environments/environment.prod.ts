c20b939d107d141369cd0f88b57f10a6f1e68b3c
frankynztein <arellanfranciree@gmail.com>
2019-08-23 12:23:40 -0500

    Imágenes para gh-pages

--- Burger-Queen/src/environments/environment.prod.ts
+++ Burger-Queen/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBurZ9LeGr7I9bR_hndK2cVbkKSojZ8pg0",
+    authDomain: "proyecto-burger-queen.firebaseapp.com",
+    databaseURL: "https://proyecto-burger-queen.firebaseio.com",
+    projectId: "proyecto-burger-queen",
+    storageBucket: "proyecto-burger-queen.appspot.com",
+    messagingSenderId: "1053122168855",
+    appId: "1:1053122168855:web:b4bba2facad0120c"
+  }
 };
